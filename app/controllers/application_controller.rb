1018e797109951bd5f3f6ecc7ea40cc4ec88c081
Farzam Azhar <farzam.azhar7777@gmail.com>
2020-10-04 02:10:51 +0500

    Handled not found exception

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
+# frozen_string_literal: true
+
 class ApplicationController < ActionController::Base
+  rescue_from ActiveRecord::RecordNotFound, with: :not_found
+
+  protected
+
+  def not_found(error = nil)
+    error = 'Not found.' if error.blank?
+
+    redirect_to root_path, alert: error
+  end
 end
